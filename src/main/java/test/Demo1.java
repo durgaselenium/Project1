de20b0eac05224d16f3454ab17a2012f2826f0c2
user <user@DESKTOP-3HBU0VV>
2022-05-16 12:32:13 +0530

    First Commit

---
+++ src/main/java/test/Demo1.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+@Test
+public void tes1() {
+System.out.println("Test1 is completed...");	
+}	
+	
+@Test
+public void tes2() {
+System.out.println("Test2 is completed...");	
+}	
+
+@Test
+public void tes3() {
+System.out.println("Test3 is completed...");	
+}
+}
